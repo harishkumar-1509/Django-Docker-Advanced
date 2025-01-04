8e822802f29a6c8731fbf9fc6895ca1a1d730cea
harishkumar-1509 <harishgandhi1509@gmail.com>
2025-01-04 12:59:16 +0530

    added tests for db connection

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,7 @@ Test custom Django management commands
 
 from unittest.mock import patch
 
-# from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -21,15 +21,15 @@ class CommandTests(SimpleTestCase):
 
         patched_check.assert_called_once_with(database=["default"])
 
-    # @patch("time.sleep")
-    # def test_wait_for_db_delay(self, patched_sleep, patched_check):
-    #     """Test waiting for database when getting OperationError"""
-    #     patched_check.side_effect = (
-    #         [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
-    #     )
+    @patch("time.sleep")
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationError"""
+        patched_check.side_effect = (
+            [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        )
 
-    #     call_command("wait_for_db")
+        call_command("wait_for_db")
 
-    #     self.assertEqual(patched_check.call_count, 6)
+        self.assertEqual(patched_check.call_count, 6)
 
-    #     patched_check.assert_called_with(database=["default"])
+        patched_check.assert_called_with(database=["default"])
